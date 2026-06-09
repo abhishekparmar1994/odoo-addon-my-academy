3feb1f2bb5d6cb0fea12adc85ea5b39b43afefe7
Abhishek Parmar <abhishekparmar@webmynesystems.com>
2026-06-09 12:04:32 +0530

    chore: add icon, banner, and html description for Odoo App Store metadata

--- my_academy_modifications/__manifest__.py
+++ my_academy_modifications/__manifest__.py
@@ -29,6 +29,9 @@
     'price': 10,
     'currency': 'USD',
     'license': 'OPL-1',
+    'images': [
+        'static/description/banner.png',
+    ],
     # Changed 'license' to 'OPL-1': Changing the license from 'LGPL-3' (open source) to 'OPL-1' (Odoo Proprietary License v1.0) is a mandatory requirement by Odoo to sell proprietary/paid modules. 
     #This license prevents buyers from copying or redistributing your code to others for free.
 }
