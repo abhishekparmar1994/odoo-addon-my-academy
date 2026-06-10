109b0933720c005bd39ffb70feb54553297d880f
Abhishek Parmar <abhishekparmar32@gmail.com>
2026-06-10 11:06:08 +0530

    Update license to OPL-1 for proprietary compliance
    Changed license from 'LGPL-3' to 'OPL-1' as required for proprietary module sales.

--- custom_addons/my_academy_modifications/__manifest__.py
+++ custom_addons/my_academy_modifications/__manifest__.py
@@ -26,7 +26,6 @@
     },
     'installable': True,
     'application': False,
-    'price': '',
     'currency': 'USD',
     'license': 'OPL-1',
     # Changed 'license' to 'OPL-1': Changing the license from 'LGPL-3' (open source) to 'OPL-1' (Odoo Proprietary License v1.0) is a mandatory requirement by Odoo to sell proprietary/paid modules. 
